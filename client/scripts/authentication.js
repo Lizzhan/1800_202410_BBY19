d282f745315bd766fb7049fdcbbe126d4b90e4ac
Lizzhan <118141838+Lizzhan@users.noreply.github.com>
2024-10-29 14:46:53 -0700

    hiding api

--- client/scripts/authentication.js
+++ client/scripts/authentication.js
@@ -1,17 +1,23 @@
 import { 
     registerBtn,
     registerEmail,
-    registerPwd
+    registerPwd,
+    registerName,
+    loginBtn,
+    loginEmail,
+    loginPwd
  } from "./dom.js";
-
  import { auth } from "./fb.js";
-
- import { createUserWithEmailAndPassword, 
-    signInWithEmailAndPassword
+ import { 
+    createUserWithEmailAndPassword, 
+    signInWithEmailAndPassword,
+    updateProfile,
+    onAuthStateChanged,
+    signOut
   } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js";
 
 
-  
+
 //register
 const register = () => {
     registerBtn.addEventListener('click', (e)=>{
@@ -25,8 +31,32 @@ const fbRegister = async () => {
     try{
         const rEmail = registerEmail.value;
         const rPwd = registerPwd.value;
-        const user = await createUserWithEmailAndPassword(auth, rEmail, rPwd,);
+        const rName = registerName.value;
+        const userCredential = await createUserWithEmailAndPassword(auth, rEmail, rPwd,);
+        console.log(userCredential);
+        await updateProfile(userCredential.user, {
+            displayName: rName,
+        });
+        console.log(userCredential.user.displayName);
         
+    }catch (err){
+        console.log(err);
+    }
+}
+
+const login = () => {
+    loginBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        fbLogin();
+    })
+}
+
+const fbLogin = async () => {
+    try{
+        const lEmail = loginEmail.value;
+        const lPwd = loginPwd.value;
+        const {user} = await signInWithEmailAndPassword(auth, lEmail, lPwd);
+        console.log(user);
     }catch (err){
         console.log(err);
     }
@@ -34,3 +64,4 @@ const fbRegister = async () => {
 
 
 register();
+login();
--- client/scripts/dom.js
+++ client/scripts/dom.js
@@ -1,15 +1,22 @@
 //creating dom elements
 const sidebar = document.getElementById('side-menu');
 const hamMenu = document.getElementById('ham-menu');
+const registerName = document.getElementById('register-name');
 const registerEmail = document.getElementById('register-username');
 const registerPwd = document.getElementById('register-password');
 const registerBtn = document.getElementById('register-button');
-
+const loginEmail = document.getElementById('login-username');
+const loginPwd = document.getElementById('login-password');
+const loginBtn = document.getElementById('login-button');
 
 export {
     hamMenu,
     sidebar,
     registerEmail,
     registerPwd,
-    registerBtn
+    registerBtn,
+    registerName,
+    loginEmail,
+    loginPwd,
+    loginBtn,
 }
